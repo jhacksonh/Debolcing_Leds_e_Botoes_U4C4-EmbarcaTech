b22e8ceca83a1865809bd3471243f1286091824d
Jacson Santos <dev.jhacks@gmail.com>
2025-01-27 23:06:39 -0300

    3º commit: Funcionalidade 1 requisitada na tarefa, implementada

--- main.c
+++ main.c
@@ -1,3 +1,28 @@
+#include "pico/stdlib.h"
+
+#define PIN_R 13
+#define TIME_PIN_R (1000/5)/2
+
+void configPins(uint pin, bool InorOut, bool pullUP);
+
 int main(){
+    configPins(PIN_R, GPIO_OUT, false);
+
+    while(1){
+        gpio_put(PIN_R, 1);
+        sleep_ms(TIME_PIN_R);
+        gpio_put(PIN_R, 0);
+        sleep_ms(TIME_PIN_R);
+    }
     return 0;
+}
+
+void configPins(uint pin, bool InorOut, bool pullUP){
+    gpio_init(pin);
+    gpio_set_dir(pin, InorOut);
+    if(!pullUP){
+        gpio_pull_up(pin);
+    }else{
+        gpio_put(pin, 0);
+    }
 }
